bf4ba5750983ca543ab6d67e7da8e219efc2d4bc
Arshad <mohammedarshad183@gmail.com>
2022-05-27 10:00:16 +0530

    Refactor: the code with Generics

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -4,7 +4,7 @@ namespace Generics
 {
     class Program
     {
-        public static void toPrint(int[] inputArray)
+        public static void toPrint<T>(T[] inputArray)
         {
             foreach (var element in inputArray)
             {
@@ -12,30 +12,15 @@ namespace Generics
             }
             Console.WriteLine("___________________________\n");
         }
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("___________________________\n");
-        }
-        public static void toPrint(string[] inputArray)
-        {
-            foreach (var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("___________________________\n");
-        }
+       
         static void Main(string[] args)
         {
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 1.2, 1.3, 1.4, 4.5 };
             string[] stringArray = { "H", "E", "L", "l", "O" };
-            Program.toPrint(intArray);
-            Program.toPrint(doubleArray);
-            Program.toPrint(stringArray);
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<string>(stringArray);
             Console.ReadLine();
         }
     }
