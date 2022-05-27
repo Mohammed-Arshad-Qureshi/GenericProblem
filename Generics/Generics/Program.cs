f6ad4874ecc2872a75fbb2b56b581321325760ed
Arshad <mohammedarshad183@gmail.com>
2022-05-27 10:39:06 +0530

    Added: three Double number

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -23,11 +23,34 @@ namespace Generics
         }
         #endregion
 
+        #region Maximum Double value
+        public static double MaximumFloatCheck(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            string maxValue = Program.MaximumCheck(3, 2, 3).ToString();
-            Console.WriteLine("The maximum of Three numbers :  " + maxValue);
-            Console.ReadLine(); 
+            int intMaxValue = Program.MaximumCheck(3, 2, 3);
+            Console.WriteLine("The maximum of Three integer numbers  :  " + intMaxValue);
+
+            double FloatMaxValue = Program.MaximumFloatCheck(1.2, 1.2, 1.3);
+            Console.WriteLine("\nThe maximum of Three double numbers   :  " + FloatMaxValue);
+            Console.ReadLine();
         }
     }
 }
