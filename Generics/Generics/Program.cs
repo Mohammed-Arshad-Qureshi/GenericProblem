c7055c5e26b6e4645e06e16403fbb63b665c71df
Arshad <mohammedarshad183@gmail.com>
2022-05-27 09:54:54 +0530

    Refactor: Given an array of Integer, Double and Character, write a program to print the same

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -4,9 +4,38 @@ namespace Generics
 {
     class Program
     {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________________\n");
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________________\n");
+        }
+        public static void toPrint(string[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("___________________________\n");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Program");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4, 4.5 };
+            string[] stringArray = { "H", "E", "L", "l", "O" };
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(stringArray);
             Console.ReadLine();
         }
     }
