e65dc4fe4c62dc5145fe75b9a477de58fedaf1b5
Arshad <mohammedarshad183@gmail.com>
2022-05-27 12:08:46 +0530

    Refactor: Code changed with Generics

---
+++ Generics/Generics/GenericMax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Generics
+{
+    public class GenericMax<T> where T : IComparable
+    {
+        #region Any type of number Maximum
+        public T MaximumAnyNumber(T firstValue, T secondValue, T thirdValue)
+        {
+            if ((firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0) ||
+               (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0) ||
+               (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0))
+            {
+                return firstValue;
+            }
+            if ((secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue).CompareTo(firstValue) >= 0 && (secondValue.CompareTo(thirdValue) > 0) ||
+                (secondValue).CompareTo(firstValue) > 0 && (secondValue.CompareTo(thirdValue) >= 0))
+            {
+                return secondValue;
+            }
+            if ((thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0) ||
+                (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0) ||
+                    (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0))
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+        #endregion
+
+      
+    }
+}
---
+++ Generics/Generics/MainClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            GenericMax<int> integerVal = new GenericMax<int>();
+            GenericMax<double> doubleVal = new GenericMax<double>();
+            GenericMax<string> stringVal = new GenericMax<string>();
+
+            int val1 = integerVal.MaximumAnyNumber(1, 2, 3);
+            double val2 = doubleVal.MaximumAnyNumber(1.1, 1.2, 1.3);
+            string val3 = stringVal.MaximumAnyNumber("1", "2", "3");
+            Console.WriteLine("Inter Max value   :  " + val1);
+            Console.WriteLine("double Max value  :  " + val2);
+            Console.WriteLine("string Max value  :  " + val3);
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+
+
+    }
+}
